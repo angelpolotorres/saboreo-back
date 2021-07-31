3f9f2f48a919fd3428d6949b0811055af2ded8e5
angelpolotorres <37557672+angelpolotorres@users.noreply.github.com>
2021-07-31 23:42:13 +0200

    eslint y prettier configurados

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,19 @@
+// Guia. Los 3 estados que puedes usar con las reglas
+const RULES = {
+  OFF: 'off',
+  WARN: 'warn',
+  ERROR: 'error',
+};
+
 module.exports = {
   env: {
-    browser: true,
     es2021: true,
-    node: true
+    node: true,
   },
-  extends: [
-    'standard'
-  ],
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  rules: {
-  }
-}
+  rules: { 'prettier/prettier': 'warn' },
+};
